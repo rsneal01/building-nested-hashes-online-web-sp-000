7425eb4afe83107ad1fdc66cf96d6983da44503a
Bobby Neal <rsneal01@gmail.com>
2020-05-08 17:19:06 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,12 +21,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        :friend_1 => {
+        {
           :name => "Steven",
           :age => "30",
           :attitude => "confused"
         },
-        :friend_2 => {
+        {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
